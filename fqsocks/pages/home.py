feb804924a71c7d229ebe7ee8faec3443e46541c
Fen Qin <fqrouter@gmail.com>
2013-10-20 00:05:20 +0800

    add ga integration

--- fqsocks/pages/home.py
+++ fqsocks/pages/home.py
@@ -3,9 +3,9 @@ import httplib
 import logging
 import os.path
 import fqlan
+import fqdns
 
 import jinja2
-import fqdns
 
 from .. import httpd
 from ..gateways import proxy_client
@@ -19,9 +19,11 @@ HOME_HTML_FILE = os.path.join(os.path.dirname(__file__), '..', 'templates', 'hom
 LOGGER = logging.getLogger(__name__)
 default_interface_ip = None
 
+
 @httpd.http_handler('GET', '')
 @httpd.http_handler('GET', 'home')
 def home_page(environ, start_response):
+    print(environ)
     global default_interface_ip
     with open(HOME_HTML_FILE) as f:
         template = jinja2.Template(unicode(f.read(), 'utf8'))
@@ -29,19 +31,22 @@ def home_page(environ, start_response):
     is_root = 0 == os.getuid()
     if not default_interface_ip:
         default_interface_ip = fqlan.get_default_interface_ip()
-    args = dict(_=environ['select_text'],
-                tcp_scrambler_enabled=HTTP_TRY_PROXY.tcp_scrambler_enabled,
-                youtube_scrambler_enabled=HTTP_TRY_PROXY.youtube_scrambler_enabled,
-                china_shortcut_enabled=proxy_client.china_shortcut_enabled,
-                direct_access_enabled=proxy_client.direct_access_enabled,
-                config=config_file.read_config(),
-                is_root=is_root,
-                default_interface_ip=default_interface_ip,
-                http_gateway=http_gateway,
-                httpd=httpd,
-                spi_wifi_repeater=downstream.spi_wifi_repeater if is_root else None)
+    args = dict(
+        _=environ['select_text'],
+        domain_name=environ.get('HTTP_HOST') or '127.0.0.1:2515',
+        tcp_scrambler_enabled=HTTP_TRY_PROXY.tcp_scrambler_enabled,
+        youtube_scrambler_enabled=HTTP_TRY_PROXY.youtube_scrambler_enabled,
+        china_shortcut_enabled=proxy_client.china_shortcut_enabled,
+        direct_access_enabled=proxy_client.direct_access_enabled,
+        config=config_file.read_config(),
+        is_root=is_root,
+        default_interface_ip=default_interface_ip,
+        http_gateway=http_gateway,
+        httpd=httpd,
+        spi_wifi_repeater=downstream.spi_wifi_repeater if is_root else None)
     html = template.render(**args).encode('utf8')
     return [html]
+
 
 @httpd.http_handler('GET', 'notice')
 def get_notice_url(environ, start_response):
@@ -51,5 +56,6 @@ def get_notice_url(environ, start_response):
         start_response(httplib.TEMPORARY_REDIRECT, [('Location', results[domain][0])])
         return []
     except:
-        start_response(httplib.TEMPORARY_REDIRECT, [('Location', 'https://s3.amazonaws.com/fqrouter-notice/index.html')])
+        start_response(httplib.TEMPORARY_REDIRECT,
+                       [('Location', 'https://s3.amazonaws.com/fqrouter-notice/index.html')])
         return []
